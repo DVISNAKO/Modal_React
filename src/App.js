c36ce8e0c3529fd1c38e8c2d738e2bb719d40927
Dima <dmitrijs.visnakovs@inbox.lv>
2023-09-07 15:10:30 +0300

    modal project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Modal from "./Modal/Modal";
 
 function App() {
+  const [modalActive, setMoadalActive] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Modal Project</h1>
+      <button className="open-btn" onClick={() => setMoadalActive(true)}>
+        click on me
+      </button>
+      <div></div>
+      <Modal active={modalActive} setActive={setMoadalActive}>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.<br></br>
+          Repellendus magnam consequuntur officiis,
+          <br></br>
+          laudantium vero quaerat corporis, hic totam iure dolores vitae
+          adipisci
+          <br></br>
+          praesentium ad nulla fugiat quos reiciendis distinctio. Error.
+        </h1>
+      </Modal>
     </div>
   );
 }
